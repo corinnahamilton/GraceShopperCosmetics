543238644cb830cef6042f2045e260177b6af3c5
elizabeth1l <lizzie.lau@gmail.com>
2022-06-15 12:57:42 -0400

    all products copmonent and reducer

--- client/App.js
+++ client/App.js
@@ -1,15 +1,16 @@
-import React from 'react'
-
-import Navbar from './components/Navbar'
-import Routes from './Routes'
+import React from "react";
+import Allproducts from "./components/Allproducts";
+import Navbar from "./components/Navbar";
+import Routes from "./Routes";
 
 const App = () => {
   return (
     <div>
-      <Navbar />
-      <Routes />
+      <Allproducts />
+      {/* <Navbar />
+      <Routes /> */}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
