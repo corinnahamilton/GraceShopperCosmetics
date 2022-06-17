e01ca2fab82c690fed57fc9d2a4c97eb98b6ad4f
Corinna Hamilton <corinnahamilton01@gmail.com>
2022-06-17 15:00:44 -0400

    create user functionality is bug free

--- client/store/createUser.js
+++ client/store/createUser.js
@@ -1,28 +1,26 @@
-import axios from "axios";
+import axios from 'axios';
 
-const CREATE_USER = "CREATE_USER";
+const CREATE_USER = 'CREATE_USER';
 
 export const _createUser = (user) => {
-    return {
-        type: CREATE_USER,
-        user,
-    };
+  return {
+    type: CREATE_USER,
+    user,
+  };
 };
 
-export const createUser = (user, history) => {
-    console.log("HISTORRY", history);
-    return async (dispatch) => {
-        const { data: created } = await axios.post("api/users", user);
-        dispatch(_createUser(created));
-        history.push("/users");
-    };
+export const createUser = (user) => {
+  return async (dispatch) => {
+    const { data: created } = await axios.post('api/users', user);
+    dispatch(_createUser(created));
+  };
 };
 
 export default function createUserReducer(state = { user: [] }, action) {
-    switch (action.types) {
-        case CREATE_USER:
-            return { users };
-        default:
-            return state;
-    }
+  switch (action.types) {
+    case CREATE_USER:
+      return { users };
+    default:
+      return state;
+  }
 }
