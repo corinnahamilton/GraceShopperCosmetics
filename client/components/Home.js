81253f7d3d0c323d4c63066ad4048c70103979f4
MT <38925772+mt20202021@users.noreply.github.com>
2022-06-19 20:01:54 -0400

    added a photo carousel to the homepage

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,19 +1,46 @@
 import React from "react";
 import { connect } from "react-redux";
+import SimpleImageSlider from "react-simple-image-slider";
 
-/**
- * COMPONENT
- */
+const images = [
+    {
+        url: "https://images.unsplash.com/photo-1560129986-baba295cf72c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1058&q=80",
+    },
+    {
+        url: "https://images.unsplash.com/photo-1613255348289-1407e4f2f980?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2030&q=80",
+    },
+    {
+        url: "https://images.unsplash.com/photo-1596704017324-04757f3e4273?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80",
+    },
+    {
+        url: "https://images.unsplash.com/photo-1551723454-7565a1f5b161?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
+    },
+    {
+        url: "https://images.unsplash.com/photo-1535683577427-740aaac4ec25?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
+    },
+];
+
+const ImageSlider = () => {
+    return (
+        <div>
+            <SimpleImageSlider
+                width={780}
+                height={960}
+                images={images}
+                showBullets={true}
+                showNavs={true}
+            />
+        </div>
+    );
+};
+
 export const Home = (props) => {
     const { username } = props;
 
     return (
         <div>
             <h3>Welcome, {username}</h3>
-            <img
-                src="https://images.unsplash.com/photo-1560129986-baba295cf72c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1058&q=80"
-                width="1024"
-            />
+            <ImageSlider />
         </div>
     );
 };
