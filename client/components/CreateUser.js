b23d7f2b33a86316a2c619097b4e3cab4644f29a
Corinna Hamilton <corinnahamilton01@gmail.com>
2022-06-21 18:40:52 -0400

    Added name to user model and sign up component

--- client/components/CreateUser.js
+++ client/components/CreateUser.js
@@ -1,76 +1,89 @@
-import React from "react";
-import { connect } from "react-redux";
-import { createUser } from "../store/createUser";
+import React from 'react';
+import { connect } from 'react-redux';
+import { createUser } from '../store/createUser';
 
 // this is our Signup component
 export class CreateUser extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: "", //???
-            password: "", // ???
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      name: '',
+      password: '', // ???
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    handleChange(event) {
-        this.setState({
-            ...this.state,
-            [event.target.name]: event.target.value,
-        });
-    }
+  handleChange(event) {
+    this.setState({
+      ...this.state,
+      [event.target.name]: event.target.value,
+    });
+  }
 
-    handleSubmit(event) {
-        event.preventDefault();
-        try {
-            this.props.createUser(this.state);
-        } catch (error) {
-            this.setState({
-                email: "",
-                password: "",
-            });
-        }
+  handleSubmit(event) {
+    event.preventDefault();
+    try {
+      this.props.createUser(this.state);
+    } catch (error) {
+      this.setState({
+        email: '',
+        name: '',
+        password: '',
+      });
     }
-    render() {
-        return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <div>
-                        <label htmlFor="email">
-                            <p>Email</p>
-                        </label>
-                        <input
-                            type="text"
-                            name="email"
-                            onChange={this.handleChange}
-                            value={this.state.email}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="password">
-                            <p>Password</p>
-                        </label>
-                        <input
-                            type="password"
-                            name="password"
-                            onChange={this.handleChange}
-                            value={this.state.password}
-                        />
-                    </div>
-                    <div>
-                        <button type="submit">Submit</button>
-                    </div>
-                </form>
-            </div>
-        );
-    }
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label htmlFor='email'>
+              <p>Email</p>
+            </label>
+            <input
+              type='text'
+              name='email'
+              onChange={this.handleChange}
+              value={this.state.email}
+            />
+          </div>
+          <div>
+            <label htmlFor='name'>
+              <p>First Name</p>
+            </label>
+            <input
+              type='name'
+              name='name'
+              onChange={this.handleChange}
+              value={this.state.name}
+            />
+          </div>
+          <div>
+            <label htmlFor='password'>
+              <p>Password</p>
+            </label>
+            <input
+              type='password'
+              name='password'
+              onChange={this.handleChange}
+              value={this.state.password}
+            />
+          </div>
+          <div>
+            <button type='submit'>Submit</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 const mapDispatchToProps = (dispatch, otherProps) => {
-    return {
-        createUser: (props) => dispatch(createUser(props, otherProps.history)),
-    };
+  return {
+    createUser: (props) => dispatch(createUser(props, otherProps.history)),
+  };
 };
 
 export default connect(null, mapDispatchToProps)(CreateUser);
--- client/components/Home.js
+++ client/components/Home.js
@@ -1,57 +1,57 @@
-import React from "react";
-import { connect } from "react-redux";
-import SimpleImageSlider from "react-simple-image-slider";
+import React from 'react';
+import { connect } from 'react-redux';
+import SimpleImageSlider from 'react-simple-image-slider';
 
 const images = [
-    {
-        url: "https://i.postimg.cc/jSCc8n2w/homeimg.png",
-    },
-    {
-        url: "https://cdn.pixabay.com/photo/2021/01/06/07/52/lipsticks-5893476_1280.jpg",
-    },
-    {
-        url: "https://images.pexels.com/photos/1161934/pexels-photo-1161934.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    },
-    {
-        url: "https://images.pexels.com/photos/6479582/pexels-photo-6479582.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    },
-    {
-        url: "https://images.pexels.com/photos/4938198/pexels-photo-4938198.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    },
-    {
-        url: "https://images.pexels.com/photos/3689976/pexels-photo-3689976.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    },
+  {
+    url: 'https://i.postimg.cc/jSCc8n2w/homeimg.png',
+  },
+  {
+    url: 'https://cdn.pixabay.com/photo/2021/01/06/07/52/lipsticks-5893476_1280.jpg',
+  },
+  {
+    url: 'https://images.pexels.com/photos/1161934/pexels-photo-1161934.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+  },
+  {
+    url: 'https://images.pexels.com/photos/6479582/pexels-photo-6479582.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+  },
+  {
+    url: 'https://images.pexels.com/photos/4938198/pexels-photo-4938198.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+  },
+  {
+    url: 'https://images.pexels.com/photos/3689976/pexels-photo-3689976.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+  },
 ];
 
 const ImageSlider = () => {
-    return (
-        <div class="slides_container">
-            <SimpleImageSlider
-                width={1280}
-                height={740}
-                images={images}
-                showBullets={true}
-                showNavs={true}
-            />
-        </div>
-    );
+  return (
+    <div class='slides_container'>
+      <SimpleImageSlider
+        width={1280}
+        height={740}
+        images={images}
+        showBullets={true}
+        showNavs={true}
+      />
+    </div>
+  );
 };
 
 export const Home = (props) => {
-    const { email } = props;
+  const { email } = props;
 
-    return (
-        <div>
-            <h1>Welcome {email} !</h1>
-            <ImageSlider />
-        </div>
-    );
+  return (
+    <div>
+      <h1>Welcome {email}!</h1>
+      <ImageSlider />
+    </div>
+  );
 };
 
 const mapStateToProps = (state) => {
-    return {
-        email: state.auth.email,
-    };
+  return {
+    email: state.auth.email,
+  };
 };
 
 export default connect(mapStateToProps)(Home);
