3b3bbb6b1b9bb38452d7f0ecb08ecc653116f165
Corinna Hamilton <corinnahamilton01@gmail.com>
2022-06-21 23:55:37 -0400

    non-logged in user cart displays message to log in to create cart

--- client/Routes.js
+++ client/Routes.js
@@ -1,19 +1,20 @@
-import React, { Component, Fragment } from "react";
-import { connect } from "react-redux";
-import { withRouter, Route, Switch, Redirect } from "react-router-dom";
-import { Login } from "./components/AuthForm";
-import Home from "./components/Home";
-import Cart from "./components/Cart";
-import SingleProduct from "./components/SingleProduct";
-import AllProducts from "./components/AllProducts";
-import Eye from "./components/Eye";
-import Face from "./components/Face";
-import Lip from "./components/Lip";
-import Nail from "./components/Nail";
-import CreateUser from "./components/CreateUser";
-import Checkout from "./components/Checkout";
-import { me } from "./store/auth";
-import Admin from "./components/Admin";
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { Login } from './components/AuthForm';
+import Home from './components/Home';
+import Cart from './components/Cart';
+import SingleProduct from './components/SingleProduct';
+import AllProducts from './components/AllProducts';
+import Eye from './components/Eye';
+import Face from './components/Face';
+import Lip from './components/Lip';
+import Nail from './components/Nail';
+import CreateUser from './components/CreateUser';
+import Checkout from './components/Checkout';
+import GuestCart from './components/GuestCart';
+import { me } from './store/auth';
+import Admin from './components/Admin';
 
 /**
  * COMPONENT
@@ -32,41 +33,41 @@ class Routes extends Component {
       <div>
         {isLoggedIn ? (
           <Switch>
-            <Route exact path="/products/eye" component={Eye} />
-            <Route exact path="/products/lip" component={Lip} />
-            <Route exact path="/products/face" component={Face} />
-            <Route exact path="/products/nail" component={Nail} />
-            <Route exact path="/signup" component={CreateUser} />
-            <Route exact path="/products" component={AllProducts} />
-            <Route exact path="/cart" component={Cart} />
-            <Route exact path="/cart/checkout/:cartId" component={Checkout} />
+            <Route exact path='/products/eye' component={Eye} />
+            <Route exact path='/products/lip' component={Lip} />
+            <Route exact path='/products/face' component={Face} />
+            <Route exact path='/products/nail' component={Nail} />
+            <Route exact path='/signup' component={CreateUser} />
+            <Route exact path='/products' component={AllProducts} />
+            <Route exact path='/cart' component={Cart} />
+            <Route exact path='/cart/checkout/:cartId' component={Checkout} />
 
-            {isAdmin && <Route exact path="/login/admin" component={Admin} />}
+            {isAdmin && <Route exact path='/login/admin' component={Admin} />}
 
             {isAdmin ? (
-              <Redirect from="/login" to="/login/admin" />
+              <Redirect from='/login' to='/login/admin' />
             ) : (
-              <Redirect from="/login" to="/" />
+              <Redirect from='/login' to='/' />
             )}
 
-            <Route exact path="/products/:id" component={SingleProduct} />
+            <Route exact path='/products/:id' component={SingleProduct} />
 
-            <Route path="/" component={Home} />
+            <Route path='/' component={Home} />
           </Switch>
         ) : (
           <Switch>
-            <Route exact path="/products/eye" component={Eye} />
-            <Route exact path="/products/lip" component={Lip} />
-            <Route exact path="/products/face" component={Face} />
-            <Route exact path="/products/nail" component={Nail} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={CreateUser} />
-            <Route exact path="/products" component={AllProducts} />
-            <Route exact path="/" component={Home} />
-            <Route exact path="/cart" component={Cart} />
+            <Route exact path='/products/eye' component={Eye} />
+            <Route exact path='/products/lip' component={Lip} />
+            <Route exact path='/products/face' component={Face} />
+            <Route exact path='/products/nail' component={Nail} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/signup' component={CreateUser} />
+            <Route exact path='/products' component={AllProducts} />
+            <Route exact path='/' component={Home} />
+            <Route exact path='/cart' component={GuestCart} />
 
-            <Route exact path="/products/:id" component={SingleProduct} />
-            <Redirect from="/logout" to="/" />
+            <Route exact path='/products/:id' component={SingleProduct} />
+            <Redirect from='/logout' to='/' />
           </Switch>
         )}
       </div>
@@ -83,7 +84,7 @@ const mapStateToProps = (state) => {
     // Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
     // if id exist, isLoggedIn is true; otherwise, false
     isLoggedIn: !!state.auth.id,
-    isAdmin: state.auth.userType == "admin",
+    isAdmin: state.auth.userType == 'admin',
   };
 };
 
--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,19 +1,19 @@
-import React from "react";
-import { connect } from "react-redux";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import {
   deleteFromCartThunk,
   getCartThunk,
   checkoutCartThunk,
-} from "../store/cart";
-import EditCart from "./EditCart";
+} from '../store/cart';
+import EditCart from './EditCart';
 
 class Cart extends React.Component {
   constructor() {
     super();
     this.state = {
       total: 0,
-      slogan: "",
+      slogan: '',
     };
     this.handleClick = this.handleClick.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -27,7 +27,7 @@ class Cart extends React.Component {
   }
   handleSubmit(cartId) {
     this.props.checkoutCart(cartId);
-    this.setState({ slogan: "Thanks" });
+    this.setState({ slogan: 'Thanks' });
     // this.props.getCart();
   }
 
@@ -67,14 +67,14 @@ class Cart extends React.Component {
                 return (
                   <div key={product.id}>
                     <span>
-                      <img src={product.imageURL} width="120" />
+                      <img src={product.imageURL} width='120' />
                     </span>
                     <span>
-                      {product.brandName} {product.productName}{" "}
+                      {product.brandName} {product.productName}{' '}
                     </span>
                     <span>${product.price}</span>
                     <button
-                      type="button"
+                      type='button'
                       value={product.id}
                       onClick={() =>
                         this.handleClick(this.props.cart.id, product.id)
@@ -98,8 +98,8 @@ class Cart extends React.Component {
             </Link>
           </div>
         ) : (
-          <div id="announcement">
-            <h1 id="announcementText">Your Cart is Empty!</h1>
+          <div id='announcement'>
+            <h1 id='announcementText'>Your Cart is Empty!</h1>
           </div>
         )}
       </div>
@@ -113,6 +113,7 @@ const mapStateToProps = (state) => {
     user: state.user,
     cart: state.cartReducer,
     cartProduct: state.cartProductReducer,
+    isLoggedIn: !!state.auth.id,
   };
 };
 
